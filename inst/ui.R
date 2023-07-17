497a69ee55a8717603a539170ada0c7ae5e6c09e
corolth1 <thibaud.coroller@novartis.com>
2023-07-17 19:44:42 +0200

    pages

--- inst/ui.R
+++ inst/ui.R
@@ -6,42 +6,34 @@ dashboardPage(
   dashboardHeader(title = "Monitoring OS"),
   # Creating tabs
   dashboardSidebar(sidebarMenu(
-    selectInput("study", "Case study:",
-                c("User","Polarix1", "Polarix2", "MonarchE", "Leda", "YTB323")),
-    textInput("events", "Events (csv)", "110, 125, 131"),
-    textInput("thres1", "Safety thresholds (csv)", "1.3, 1.2, 1"),
-    textInput("thres2", "Stopping thresholds (csv)", "NULL"),
-    textInput("hrs", "True HRs", "0.7, 1, 1.1, 1.2, 1.5"),
-    selectInput("method", "Method :", c("joint", "cond"))
-    # Tab-1
-    # menuItem(
-    #   "Run",
-    #   tabName = "run",
-    #   icon = icon("person-running"),
-    #   selectInput("study", "Case study/User specified parameter:",
-    #               c("User input","polarix1", "polarix2", "MonarchE", "Leda")),
-    #   textInput("events", "Events (csv)", "110, 125, 131"),
-    #   textInput("thres1", "Safety thresholds (csv)", "1.3, 1.2, 1"),
-    #   textInput("thres2", "Stop thresholds (csv)", "1.5, 1.4, 1.3"),
-    #   selectInput("method", "Method :", c("joint", "cond"))
-    # )
-  )
+    menuItem("Welcome", tabName = "Welcome", icon = shiny::icon("info-circle")),
+    menuItem("Settings", tabName = "Parameters", icon = shiny::icon("tachometer-alt")),
+    menuItem("Calculation", tabName = "Results", icon = shiny::icon("square-poll-vertical"))
+    )
   ),
   # Define body
-  dashboardBody(tabItems(
-    # First tab content
-    tabItem(tabName = "Modeling", class = "active"),
-    # empty for now
-    # Second tab content
-    tabItem(
-      tabName = "Sampling",
-      class = "active",
-      fluidRow(
-        shinydashboard::box(status = "primary", plotOutput("plot1", height = "512px")),
-        shinydashboard::box(status = "success", plotOutput("plot2", height = "512px")),
-        shinydashboard::box(status = "primary", tableOutput("table1")),
-        shinydashboard::box(status = "success", tableOutput("table2"))
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "Welcome", shiny::includeMarkdown(glue('{pkgload::pkg_path()}/README.md'))),
+      tabItem(tabName = "Parameters",
+              fluidRow(
+                column(3, selectInput("study", "Case study:",
+                            c("User","Polarix1", "Polarix2", "MonarchE", "Leda", "YTB323"))),
+                column(3, textInput("events", "Events (csv)", "110, 125, 131"))),
+              fluidRow(
+                column(3, textInput("thres1", "Safety thresholds (csv)", "1.3, 1.2, 1")),
+                column(3, textInput("thres2", "Stopping thresholds (csv)", "NULL")),
+                column(3, textInput("hrs", "True HRs", "0.7, 1, 1.1, 1.2, 1.5")),
+                column(3, selectInput("method", "Method :", c("joint", "cond"))),
+                )
+              ),
+      tabItem(tabName = "Results", fluidRow(
+        shinydashboard::box(status = "primary", plotOutput("prob_plot", height = "512px")),
+        shinydashboard::box(status = "success", plotOutput("flplot", height = "512px")),
+        shinydashboard::box(status = "primary", tableOutput("ocs_trial")),
+        shinydashboard::box(status = "success", tableOutput("ocs_stage"))
+        )
       )
     )
-  ))
+  )
 )
