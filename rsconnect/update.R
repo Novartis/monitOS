8712024091aaa7e0bb11404271325c282d516559
COROLTH1 <thibaud.coroller@novartis.com>
2023-08-23 18:49:46 +0200

    cleanup

--- rsconnect/update.R
+++
@@ -1,6 +0,0 @@
-# # ### for internal app, use below code
-rsconnect::deployApp(appDir = here::here(),
-                     appId = 2946,
-                     account = "corolth1",
-                     server = "rsconnect-prod.dit.eu.novartis.net")
-
--- server.sh
+++
@@ -1,6 +0,0 @@
-rsconnect add \
---api-key XIyGQEDhReuQpMMEHzHny1vBN1wM2PAg \
---server rsconnect-prod.dit.eu.novovartis.net \
---name davinci \
---cacert ~/Rconnect/certs/silverthree-chain.pem
-
